43bdca5d1ae88379645a45a3ab12e557c6389f08
Eric Anderson <eric@vector241.com>
2016-07-11 08:39:59 -0500

    Initial code commit of application.

---
+++ OpenSolution.bat
@@ -0,0 +1,1 @@
+start src\SimpleCalculator.sln 
---
+++ src/SimpleCalculator/Controllers/CalculateController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace SimpleCalculator.Controllers
+{
+    public class CalculateController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
